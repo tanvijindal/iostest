fc62bf7560a5ab74b77dde8a982fe29d53e7dcec
Tanvi <tanvi@Tanvis-MacBook-Pro.local>
2013-09-25 16:01:28 -0500

    Initial Commit

---
+++ Ratings/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ratings
+//
+//  Created by Tanvi on 9/25/13.
+//  Copyright (c) 2013 Tanvi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Ratings/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Ratings
+//
+//  Created by Tanvi on 9/25/13.
+//  Copyright (c) 2013 Tanvi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
